40f3adbb2ff0863fa2c08c2589e47e28d6c60e9a
dafatov <33876950+dafatov@users.noreply.github.com>
2021-10-30 15:14:22 +0300

    Add files via upload

--- VkHider.user.js
+++ VkHider.user.js
@@ -4,13 +4,14 @@
 // @version      0.1
 // @description  Delete shit from vk.com
 // @author       dafatov
-// @match        https://*.vk.com/im*
-// @match        http://*.vk.com/im*
+// @match        https://vk.com/im*
+// @match        http://vk.com/im*
 // @license      MIT
 // @grant        none
 // ==/UserScript==
 
-const debug = false;
+const debug = true;
+var shit;
 
 function log(log) {
     if (!debug) {
@@ -18,14 +19,44 @@ function log(log) {
     }
 }
 
-(function() {
-    'use strict';
+var omMutate = function(mutationsList) {
+    shit = document.querySelector("#im_dialogs > ul");
+    if (shit == null || shit.style.display == 'none') return;
+    log(mutationsList);
 
-    var shit = document.querySelector("#im_dialogs > ul");
-
-    if (shit === null) {
-        log('Already removed');
-        return;
+    for (var i=0; i < mutationsList.length; i++) {
+        var addedNodes = mutationsList[i].addedNodes[0];
+        if (addedNodes) {
+            var attr = addedNodes.getAttribute('data-list-id');
+            if (attr) {
+                shit.style.display = 'none';
+                log("shit noned mutateble");
+            }
+        }
     }
-    shit.remove();
-})();
+}
+
+function doSmth() {
+    shit = document.querySelector("#im_dialogs > ul");
+    var container = document.querySelector("body");
+
+    var observer = new MutationObserver(omMutate);
+    observer.observe(container, { attributes: true, childList: true, subtree: true, characterData: true,
+    characterDataOldValue: true });
+    shit.style.display = 'none';
+    log("shit noned");
+}
+
+
+function ready(fn) {
+    document.addEventListener('page:load', fn);
+    document.addEventListener('turbolinks:load', fn);
+
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(doSmth);
